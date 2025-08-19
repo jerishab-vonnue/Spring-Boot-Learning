0cb320d9538e3bb97328b0f1649d368e6d7d8474
jerishab-vonnue <jerishab.m@vonnue.com>
2025-08-19 11:08:22 +0530

    feat: image post and get

--- ecom-project/src/main/java/com/jerishab/ecom_project/controller/ProductController.java
+++ ecom-project/src/main/java/com/jerishab/ecom_project/controller/ProductController.java
@@ -5,8 +5,10 @@ import com.jerishab.ecom_project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -37,6 +39,32 @@ public class ProductController {
             else
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND) ;
         }
+
+
+        @PostMapping("/product")
+        public  ResponseEntity<?> addProduct(@RequestPart Product product,
+                                             @RequestPart MultipartFile imageFile){
+            try {
+                System.out.println(product);
+                Product product1 = service.addProduct(product,imageFile);
+                return  new ResponseEntity<>(product1,HttpStatus.CREATED);
+            }catch (Exception e){
+                return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+
+            }
+        }
+
+        @GetMapping("/product/{productId}/image")
+        public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId){
+            Product product =service.getProductById(productId);
+            byte[] imageFile = product.getImageDate();
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.valueOf(product.getImageType()))
+                    .body(imageFile);
+        }
+
+
 
 
 }
--- ecom-project/src/main/java/com/jerishab/ecom_project/model/Product.java
+++ ecom-project/src/main/java/com/jerishab/ecom_project/model/Product.java
@@ -1,13 +1,9 @@
 package com.jerishab.ecom_project.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Generated;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
@@ -22,13 +18,19 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
-    private String desc;
+    private String description;
     private String brand;
     private BigDecimal price;
     private String category;
-    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy")
+
     private Date releaseDate;
-    private boolean availabe;
-    private int quantity;
+    private boolean productAvailable;
+    private int stockQuantity;
+
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageDate;
+
 
 }
